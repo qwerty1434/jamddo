64fabbb6b227c932ceff534eff7ee529dce2327e
qwerty1434 <qwerty1434@naver.com>
2022-11-29 23:12:20 +0900

    :sparkles: [FEAT] axios통신기능 구현
    백엔드와의 통신으로 데이터를 주고받을 수 있게 됨

--- frontend/jamddo/src/views/SimulationView.vue
+++ frontend/jamddo/src/views/SimulationView.vue
@@ -1,11 +1,34 @@
 <template>
   <div class="home">
     <h1>여기는 시뮬레이션</h1>
+    <button v-on:click="func1">로또1장 구매</button>
+    <div>{{ func1_data }}</div>
   </div>
 </template>
 
 <script>
+const addr = "http://localhost:8080/simulation";
+import axios from "axios";
 export default {
   name: "HomeView",
+  data() {
+    return {
+      func1_data: "",
+      func2_data: "",
+    };
+  },
+  methods: {
+    func1: function () {
+      axios
+        .get(addr + "/buyOne")
+        .then((response) => {
+          console.log(response.data);
+          this.func1_data = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+  },
 };
 </script>
