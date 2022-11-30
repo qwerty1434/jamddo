0e3572b63246e735a52e5429c618e5eb4539c90b
qwerty1434 <qwerty1434@naver.com>
2022-11-30 21:24:31 +0900

    :sparkles: [FEAT] 백엔드와 API통신 구현
    지금까지 구현된 API 모두 연결 완료

--- frontend/jamddo/src/views/SimulationView.vue
+++ frontend/jamddo/src/views/SimulationView.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="home">
     <h1>여기는 시뮬레이션</h1>
-    <button v-on:click="func1">로또1장 구매</button>
-    <div>{{ func1_data }}</div>
+    <button v-on:click="buyOne">로또1번 구매</button>
+    <div>{{ buyOneData }}</div>
+
+    <button v-on:click="buyBundle()">로또N번 구매</button>
+    <div>{{ buyBundleData }}</div>
+
+    <button v-on:click="buyUntilFirstPlace">1등 나올때까지 구매</button>
+    <div>{{ buyUntilFirstPlaceData }}</div>
   </div>
 </template>
 
@@ -13,17 +19,40 @@ export default {
   name: "HomeView",
   data() {
     return {
-      func1_data: "",
-      func2_data: "",
+      buyOneData: "",
+      buyBundleData: "",
+      buyUntilFirstPlaceData: "",
     };
   },
   methods: {
-    func1: function () {
+    buyOne() {
       axios
         .get(addr + "/buyOne")
         .then((response) => {
           console.log(response.data);
-          this.func1_data = response.data;
+          this.buyOneData = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    buyBundle() {
+      var Cnt = prompt("시행횟수를 입력해 주세요", 0);
+      axios
+        .get(addr + "/buyBundle/" + Cnt)
+        .then((response) => {
+          console.log(response.data);
+          this.buyBundleData = response.data;
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    },
+    buyUntilFirstPlace() {
+      axios
+        .get(addr + "/untilFirstPlace")
+        .then((response) => {
+          this.buyUntilFirstPlaceData = response.data;
         })
         .catch((error) => {
           console.log(error);
--- frontend/jamddo/src/views/StatisticView.vue
+++ frontend/jamddo/src/views/StatisticView.vue
@@ -1,12 +1,72 @@
 <template>
   <div class="home">
     <h1>여기는 통계</h1>
+
+    <h2>이번주 1위 당첨정보</h2>
+    <div>{{ WinningNumData }}</div>
+
+    <h2>이번주 회차, 당첨번호, 상금, 당첨자 수</h2>
+    <div>{{ overviewData }}</div>
+
+    <h2>당첨번호 통계</h2>
+    <div>{{ numStatisticData }}</div>
+
+    <h2>당첨번호 통계 - 색상별로 묶기</h2>
+    <div>{{ ColorStatisticData }}</div>
   </div>
 </template>
 
 <script>
+const addr = "http://localhost:8080/info";
+import axios from "axios";
 export default {
   name: "HomeView",
-  created: {},
+  data() {
+    return {
+      WinningNumData: "",
+      overviewData: "",
+      numStatisticData: "",
+      ColorStatisticData: "",
+    };
+  },
+  created() {
+    axios
+      .get(addr + "/winningNum")
+      .then((response) => {
+        console.log(response.data);
+        this.WinningNumData = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(addr + "/overview")
+      .then((response) => {
+        console.log(response.data);
+        this.overviewData = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(addr + "/statistics/number")
+      .then((response) => {
+        console.log(response.data);
+        this.numStatisticData = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    axios
+      .get(addr + "/statistics/number/color")
+      .then((response) => {
+        console.log(response.data);
+        this.ColorStatisticData = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+  methods: {},
 };
 </script>
