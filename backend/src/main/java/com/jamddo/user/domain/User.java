8c80c825989e41f5c74f855c355e0c7494f2221c
qwerty1434 <qwerty1434@naver.com>
2022-12-02 20:30:42 +0900

    :sparkles: [FEAT] 유저 도메인 생성
    유저 도메인 생성

---
+++ backend/src/main/java/com/jamddo/user/domain/User.java
@@ -0,0 +1,21 @@
+package com.jamddo.user.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long id;
+
+    private String nickname;
+
+}
