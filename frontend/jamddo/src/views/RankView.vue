2930305a3128490959d8261119cd94c20535ed6a
qwerty1434 <qwerty1434@naver.com>
2023-01-10 19:11:04 +0900

    :sparkles: [FEAT] 랭킹페이지에서 데이터 받아오기 작성
    axios통신으로 값을 받아오는 작업 완료

--- frontend/jamddo/src/views/RankView.vue
+++ frontend/jamddo/src/views/RankView.vue
@@ -1,6 +1,28 @@
 <template>
-  <div>랭킹페이지</div>
+  <div>
+    랭킹페이지
+    {{ RankingData }}
+  </div>
 </template>
 <script>
-export default {};
+const addr = "http://localhost:8080/user";
+import axios from "axios";
+export default {
+  name: "RankView",
+  data() {
+    return {
+      RankingData: "",
+    };
+  },
+  created() {
+    axios
+      .get(addr + "/ranking")
+      .then((response) => {
+        this.RankingData = response.data;
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+};
 </script>
