110f48b76b9b19cc4e1cdf835a3f5e2f35c470c1
qwerty1434 <qwerty1434@naver.com>
2023-03-08 22:37:18 +0900

    :fire: [REMOVE] speetto기능 삭제
    해당 기능 없이 기본기능만 유지하기로 함

--- backend/src/test/java/com/jamddo/speetto/service/SpeettoServiceTest.java
+++ backend/src/test/java/com/jamddo/speetto/service/SpeettoServiceTest.java
@@ -3,6 +3,7 @@ package com.jamddo.speetto.service;
 import com.jamddo.speetto.domain.Speetto;
 import com.jamddo.speetto.dto.SpeettoDto;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,18 +22,23 @@ class SpeettoServiceTest {
     public void concurrentTest(){
         Runnable userA = () -> {
             speettoService.buyOne();
+            log.info("Thread A"+speettoService.getData().totalRemainCnt());
         };
         Thread threadA = new Thread(userA);
         threadA.start();
 
         Runnable userB = () -> {
             speettoService.buyOne();
+            log.info("Thread B"+speettoService.getData().totalRemainCnt());
+
         };
         Thread threadB = new Thread(userB);
         threadB.start();
 
         Runnable userC = () -> {
             speettoService.buyOne();
+            log.info("Thread C"+speettoService.getData().totalRemainCnt());
+
         };
         Thread threadC = new Thread(userC);
         threadC.start();
@@ -41,6 +47,8 @@ class SpeettoServiceTest {
         SpeettoDto speettoDto = speettoService.getData();
         log.info("speetto.totalRemainCnt() = {}", speettoDto.totalRemainCnt());
         Assertions.assertEquals(39999997,speettoDto.totalRemainCnt());
+
+
     }
 
     private void sleep(int millis){
