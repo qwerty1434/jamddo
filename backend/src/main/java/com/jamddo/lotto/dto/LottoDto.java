e2df888087adaaffe6a9aefe2df6eb253d024d43
qwerty1434 <qwerty1434@naver.com>
2023-05-14 02:01:06 +0900

    :white_check_mark: 로또번호가 유효한 숫자들인지에 대한 검증
    LottoDto에 array로 변환하는 코드 추가

--- backend/src/main/java/com/jamddo/lotto/dto/LottoDto.java
+++ backend/src/main/java/com/jamddo/lotto/dto/LottoDto.java
@@ -16,5 +16,8 @@ public class LottoDto {
     private int fourthNum;
     private int fifthNum;
     private int sixthNum;
+    public int[] getArray(){
+        return new int[]{firstNum, secondNum, thirdNum, fourthNum, fifthNum, sixthNum};
+    }
 
 }
