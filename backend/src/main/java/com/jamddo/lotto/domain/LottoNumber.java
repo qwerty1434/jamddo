667329622a236d5db30f450bcb48c74b523d0c47
qwerty1434 <qwerty1434@naver.com>
2023-06-14 14:47:11 +0900

    :sparkles: refactor: LottoNumber 도메인 생성
    단순한 1~45사이 숫자가 아니라 특별한 의미를 지니는 값이기 때문에 도메인으로 추출

---
+++ backend/src/main/java/com/jamddo/lotto/domain/LottoNumber.java
@@ -0,0 +1,34 @@
+package com.jamddo.lotto.domain;
+
+import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class LottoNumber {
+    private static final List<LottoNumber> LOTTO_NUMBER_LIST;
+    private static final Map<Integer,LottoNumber> LOTTO_NUMBER_MAP = new HashMap<>();
+
+    private final int lottoNumber;
+
+    public LottoNumber(int lottoNumber) {
+        this.lottoNumber = lottoNumber;
+    }
+    static{
+        LOTTO_NUMBER_LIST = IntStream.rangeClosed(1,45)
+                .boxed()
+                .map(LottoNumber::new)
+                .collect(Collectors.toList());
+
+        LOTTO_NUMBER_LIST.stream()
+                .forEach(v -> LOTTO_NUMBER_MAP.put(v.lottoNumber, v));
+    }
+
+    public static List<LottoNumber> getLottoNumbers(int cnt){
+        Collections.shuffle(LOTTO_NUMBER_LIST);
+        return LOTTO_NUMBER_LIST.subList(0,cnt);
+    }
+
+    public int getLottoNumber(){
+        return lottoNumber;
+    }
+}
