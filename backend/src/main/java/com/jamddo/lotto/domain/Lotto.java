8b63aeda143e23222f9c03ea4e2df4fd6f3f98b9
qwerty1434 <qwerty1434@naver.com>
2023-06-20 17:18:03 +0900

    :recycle: 회차 정보 추가

--- backend/src/main/java/com/jamddo/lotto/domain/Lotto.java
+++ backend/src/main/java/com/jamddo/lotto/domain/Lotto.java
@@ -3,12 +3,22 @@ package com.jamddo.lotto.domain;
 import com.jamddo.lotto.domain.history.LottoHistory;
 
 public class Lotto {
-    int round;
+    long round;
     private final LottoNumbers lottoNumbers;
 
     public Lotto() {
+        this.round = generateRound();
         this.lottoNumbers = new LottoNumbers();
     }
+
+    private long generateRound(){
+        final long ONE_WEEK = 1000 * 60 * 60 * 24 * 7;
+        final long START_DATE = 1_038_654_000_000L;
+        final long NOW_DATE = System.currentTimeMillis();
+        return ((NOW_DATE - START_DATE) / ONE_WEEK +
+                (((NOW_DATE - START_DATE) % ONE_WEEK == 0) ? 0 : 1));
+    }
+
     public Rank getRank(LottoHistory lottoHistory){
         int matchCnt = getMatchCnt(lottoHistory.getLottoNumbers());
         boolean isBonus = isContainBonusNumber(lottoHistory.getBonusNumber());
@@ -27,7 +37,7 @@ public class Lotto {
     }
 
 
-    public int getRound() {
+    public long getRound() {
         return round;
     }
 
